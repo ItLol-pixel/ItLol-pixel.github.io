480525cd4472ee617214e96cac7d4e45223dcb81
ItLol-pixel <olechka.tischenko@gmail.com>
2020-06-02 19:16:17 +0300

    events-handling-stop

--- Handling_Events/event.js
+++ Handling_Events/event.js
@@ -1,28 +1,28 @@
 var justdiv = document.getElementById("justdiv")
-var mouseColor = {
+var mouseColor1 = {
     "mouseover": "yellow",
     "click": "red",
     "dblclick": "orange",
     "mouseout": "transparent"
 }
 
-for (var etype in mouseColor) {
+for (var etype in mouseColor1) {
     justdiv.addEventListener(etype, handler);
 }
 function handler(e) {
-    e.target.style.backgroundColor = mouseColor[e.type];
+    e.target.style.backgroundColor = mouseColor1[e.type];
 }
 var justdiv2 = document.getElementById("justdiv2")
 var mouseColor = {
     "mouseover": "aqua",
-    "click": "red",
+    "click": "lime",
     "dblclick": "brown",
     "mouseout": "pink"
 }
 
 for (var etype in mouseColor) {
-    justdiv2.addEventListener(etype, handler);
+    justdiv2.addEventListener(etype, handler2);
 }
-function handler(e) {
-    e.target.style.backgroundColor = mouseColor[e.type];
+function handler2(e) {
+    e.target.style.backgroundColor = mouseColor[e.type]; e.stopPropagation();
 }

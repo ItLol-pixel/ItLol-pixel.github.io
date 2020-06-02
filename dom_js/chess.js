c5ec456027aee1c246db09cd404e160f969d4407
ItLol-pixel <olechka.tischenko@gmail.com>
2020-06-02 13:35:33 +0300

    black-white-chess

--- dom_js/chess.js
+++ dom_js/chess.js
@@ -1,16 +1,16 @@
-var content = document.getElementById('content');
-for (var i = 0; i <= 8; i++) {
-    var newRow = document.createElement('div');
-    newRow.setAttribute('class', 'row');
+var container = document.getElementById("content");
+for (var i = 1; i <= 8; i++) {
+    var newRow = document.createElement("div");
+    newRow.setAttribute("class", "row");
     for (var j = 1; j <= 8; j++) {
-        var newCell = document.createElement('div');
-        newCell.setAttribute('class', 'cell');
+        var newCell = document.createElement("div");
+        var _classblwh = "";
+        _classblwh =
+            (i % 2 == 0 && j % 2 !== 0) || (i % 2 !== 0 && j % 2 == 0)
+                ? "cell cell_black"
+                : "cell cell_white";
+        newCell.setAttribute("class", _classblwh);
         newRow.appendChild(newCell);
-        if (i % 2 == j % 2) {
-            newCell.setAttribute("class", "white cell")
-        } else {
-            newCell.setAttribute("class", "black cell")
-        }
     }
-    content.appendChild(newRow);
+    container.appendChild(newRow);
 }

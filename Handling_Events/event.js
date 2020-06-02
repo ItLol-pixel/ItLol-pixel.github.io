7a7c701ed48d3f620cbb818d27c8d58194b7adb8
ItLol-pixel <olechka.tischenko@gmail.com>
2020-06-02 20:50:26 +0300

    update2050

--- Handling_Events/event.js
+++ Handling_Events/event.js
@@ -1,28 +1,28 @@
 var justdiv = document.getElementById("justdiv")
-var mouseColor1 = {
+var mouseColor = {
     "mouseover": "yellow",
     "click": "red",
     "dblclick": "orange",
     "mouseout": "transparent"
 }
 
-for (var etype in mouseColor1) {
+for (var etype in mouseColor) {
     justdiv.addEventListener(etype, handler);
 }
 function handler(e) {
-    e.target.style.backgroundColor = mouseColor1[e.type];
+    e.target.style.backgroundColor = mouseColor[e.type];
 }
 var justdiv2 = document.getElementById("justdiv2")
-var mouseColor = {
+var mouseColor2 = {
     "mouseover": "aqua",
     "click": "lime",
     "dblclick": "brown",
     "mouseout": "pink"
 }
 
-for (var etype in mouseColor) {
+for (var etype in mouseColor2) {
     justdiv2.addEventListener(etype, handler2);
 }
 function handler2(e) {
-    e.target.style.backgroundColor = mouseColor[e.type]; e.stopPropagation();
+    e.target.style.backgroundColor = mouseColor2[e.type]; e.stopPropagation();
 }

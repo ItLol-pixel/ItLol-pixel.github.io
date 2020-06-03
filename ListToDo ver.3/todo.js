bb8a94712441c2fddc7d336c3229bed1029340ac
ItLol-pixel <olechka.tischenko@gmail.com>
2020-06-03 10:01:39 +0300

    listtodo -ver.3

--- ListToDo ver.3/todo.js
+++ ListToDo ver.3/todo.js
@@ -1,53 +1,50 @@
-//получаем объекты - поле ввода input, кнопку button, ul//
-let addMessage = document.querySelector('.message'),
-    addButton = document.querySelector(' .add');
-todo = document.querySelector('.todo');
+btnAdd.addEventListener("click", addTodoItem, false);
+todoitem.addEventListener("keydown", addTodoItem, false);
+btnSave.addEventListener("click", saveTodoList, false);
+date = document.querySelector(".date");
 
-//объявляем переменную массив, в который будет сохраняться весь список дел**//
-let todoList = [];
-//возвращаем данные на страницу из localStorage: в случае обновления страницы ранее внесенные задачи не будут утеряны//
-if (localStorage.getItem('todo')) {
-    todoList = JSON.parse(localStorage.getItem('todo'));
-    displayMessages();
+var today = new Date().toLocaleDateString();
+date.textContent = today;
+
+var todoItems = [];
+
+function addTodoItem(e) {
+    if ((e.type == "keydown") && (e.keyCode != 13)) {
+        return;
+    }
+    if (todoitem.value.length < 1) {
+        return;
+    }
+    if (!todoItems.includes(todoitem.value)) {
+        var newTodoItem = document.createElement("p");
+        newTodoItem.innerText = todoitem.value;
+
+        newTodoItem.addEventListener("click", todoItemDone, false);
+        newTodoItem.addEventListener("dblclick", todoItemRemove, false)
+
+        list.appendChild(newTodoItem);
+        todoItems.push(todoitem.value);
+        todoitem.value = "";
+    }
 }
-//создаем обработчик событий при введении в поле input новой информации и нажатии на кнопку button//
-addButton.addEventListener('click', function () {
-    if (!addMessage.value) return;
-    //создаем массив при добавлении новых задач с набором св-в//
-    let newTodo = {
-        todo: addMessage.value,
-        checked: false,
-        important: false
-    };
-    todoList.push(newTodo);
-    displayMessages();
-    //сохранение данных при помощи localStorage, передавая туда наш объект todoList (для этого объект преобразовываем в JSON строку)//
-    localStorage.setItem('todo', JSON.stringify(todoList));
-    //после ввода данных и их добавления, кликаем в поле ввода  - строка очищается- как сделать???//
 
-});
-//создаем ф-цию которая будет перебирать элементы массива и выводить список дел//
-function displayMessages() {
-    let displayMessage = '';
-    todoList.forEach(function (item, i) {
-        //**при помощи интерполяции в строке создаётся конструкция ${...}, внутри которой можно поместить любую переменную или выражение */
-        displayMessage += `
-        <li>
-        <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-        <label for='item_${i}' class="${item.important ? 'important' : ''}>${item.todo}</label>
-        </li>
-        `;
-        todo.innerHTML = displayMessage;
-    });
-    todo.addEventListener('contextmenu', function (event) {
-        event.preventDefault();
-        //при клике правой кнопки мыши вместо контекст меню будет перебираться весь массив//
-        todoList.forEach(function (item) {
-            if (item.todo === event.target.innerHTML) {
-                item.important = !item.important;
-                displayMessages();
-                localStorage.setItem('todo', JSON.stringify(todoList));
-            }
-        });
-    });
+function todoItemDone(e) {
+    if (e.target.style.textDecoration == "line-through") {
+        e.target.style.textDecoration = "";
+    } else {
+        e.target.style.textDecoration = "line-through";
+    }
+}
+
+function todoItemRemove(e) {
+    if (confirm("Do you really want to remove the item?")) {
+        e.target.remove();
+    }
+}
+
+function saveTodoList(e) {
+    if (todoItems.length != 0) {
+        localStorage.setItem("todolist", JSON.stringify(todoItems));
+        window.alert("TodoList has been saved");
+    }
 }
